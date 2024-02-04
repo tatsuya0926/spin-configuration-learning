2750c3f5ae67a6f7f169662b33e5ce87633570e8
Tatsuya MIYATA <miyata.tatsuya1209@gmail.com>
2024-02-05 01:52:13 +0900

    cnn model update, fixed utils.py

--- learning/train/utils.py
+++ learning/train/utils.py
@@ -60,11 +60,16 @@ def create_train_data_hold_out(
     T_cr_1,
     exclude_T,
     total_label,
-    split_num,
+    model_name,
+    L,
     Q=None,
     T_cr_2=None,
-    normalize=False
+    normalize=False,
+    correlation_configuration=False
 ):
+    """
+    hold out用データ作成&分割メソッド
+    """
     train_dataset, valid_dataset, merge_valid_dataset, exclude_dataset = [], [], [], []
     if total_label == 2:
         (t_start1, t_end1) = exclude_T
@@ -94,22 +99,19 @@ def create_train_data_hold_out(
 
         for itrj in range(ndata):
             npsc = np.load(f"{fname}{itrj}.npy")
+            if correlation_configuration:
+                npsc = calc_correlation_configuration(npsc, L, model_name, Q)
             if (normalize):
                 npsc = npsc / Q
             if condition:
                 exclude_dataset.append(
                     (torch.tensor(npsc, dtype=torch.float32).unsqueeze(0), temp, label))
-                if itrj == split_num:
-                    break
             else:
                 if itrj < int(ndata*0.7):
-                    train_dataset.append(
-                        (torch.tensor(npsc, dtype=torch.float32).unsqueeze(0), temp, label))
+                    train_dataset.append((torch.tensor(npsc, dtype=torch.float32).unsqueeze(0), temp, label))
                 else:
-                    valid_dataset.append(
-                        (torch.tensor(npsc, dtype=torch.float32).unsqueeze(0), temp, label))
-                    merge_valid_dataset.append(
-                        (torch.tensor(npsc, dtype=torch.float32).unsqueeze(0), temp, label))
+                    valid_dataset.append((torch.tensor(npsc, dtype=torch.float32).unsqueeze(0), temp, label))
+                    merge_valid_dataset.append((torch.tensor(npsc, dtype=torch.float32).unsqueeze(0), temp, label))
     merge_valid_dataset.extend(exclude_dataset)
     merge_valid_dataset = sorted(
         merge_valid_dataset, reverse=False, key=lambda x: x[1])
@@ -121,31 +123,37 @@ def create_train_data_CV(
     prm_list,
     ndata,
     T_cr_1,
-    exclude_T,
     total_label,
-    split_num,
+    model_name,
+    L,
+    exclude_T=None,
     Q=None,
     T_cr_2=None,
-    normalize=False
+    normalize=False,
+    correlation_configuration=False
 ):
+    """
+    cross validation用データ作成&分割メソッド
+    exclude_T = (t_start1, t_end1), (t_start1, t_end1, t_start2, t_end2)を設定するならば, exclude_T内の温度はdatasetから除外される. exclude_Tを設定しないのであれば, datasetに全データを格納する.
+    normalize=Trueなら行列を正規化する. 
+    """
     dataset, exclude_dataset = [], []
-    if total_label == 2:
+    if total_label == 2 and exclude_T != None:
         (t_start1, t_end1) = exclude_T
-    elif total_label == 3:
+    elif total_label == 3 and exclude_T != None:
         (t_start1, t_end1, t_start2, t_end2) = exclude_T
     else:
         print("Please set argument:'total_label'")
     for itemp in range(len(prm_list)):
         temp, fname = prm_list[itemp]
         if total_label == 2:
-            condition = temp >= t_start1 and temp <= t_end1
+            if exclude_T != None: condition = temp >= t_start1 and temp <= t_end1
             if temp < T_cr_1:
                 label = 0
             else:
                 label = 1
         elif total_label == 3:
-            condition = (temp >= t_start1 and temp <= t_end1) or (
-                temp >= t_start2 and temp <= t_end2)
+            if exclude_T != None: condition = (temp >= t_start1 and temp <= t_end1) or (temp >= t_start2 and temp <= t_end2)
             if temp < T_cr_1:
                 label = 0
             elif temp > T_cr_1 and temp < T_cr_2:
@@ -153,42 +161,46 @@ def create_train_data_CV(
             else:
                 label = 2
         else:
-            print("Please set argument:'total_label'")
+            print("all data add dataset array.")
+            print("if you want to exclude T, please set argument:'total_label'")
+            
 
         for itrj in range(ndata):
             npsc = np.load(f"{fname}{itrj}.npy")
-            if (normalize):
+            if correlation_configuration:
+                npsc = calc_correlation_configuration(npsc, L, model_name, Q)
+            if normalize:
                 npsc = npsc / Q
-            if condition:
-                exclude_dataset.append(
-                    (torch.tensor(npsc, dtype=torch.float32).unsqueeze(0), temp, label))
-                if itrj == split_num:
-                    break
+            if exclude_T != None:
+                if condition:
+                    exclude_dataset.append((torch.tensor(npsc, dtype=torch.float32).unsqueeze(0), temp, label))
+                else:
+                    dataset.append((torch.tensor(npsc, dtype=torch.float32).unsqueeze(0), temp, label))
             else:
-                dataset.append(
-                    (torch.tensor(npsc, dtype=torch.float32).unsqueeze(0), temp, label))
+                dataset.append((torch.tensor(npsc, dtype=torch.float32).unsqueeze(0), temp, label))
+                
 
     return dataset, exclude_dataset
 
 
 def inference(total_test_dataset, temps, prediction_test, target_size):
+    """
+    推論用メソッド
+    """
     xs, y1s, y2s, y3s = [], [], [], []
     sum_pred_0, sum_pred_1, sum_pred_2 = 0, 0, 0
     count = 0
 
     for i in range(total_test_dataset):
         if i == 0:
-            sum_pred_0, sum_pred_1, sum_pred_2 = __pred_count(
-                sum_pred_0, sum_pred_1, sum_pred_2, prediction_test[i])
+            sum_pred_0, sum_pred_1, sum_pred_2 = __pred_count(sum_pred_0, sum_pred_1, sum_pred_2, prediction_test[i])
             count += 1
             xs.append(temps[i])
         else:
             if temps[i] != temps[i-1]:
-                # 格納
                 y1s.append(sum_pred_0/count)
                 y2s.append(sum_pred_1/count)
-                if target_size == 3:
-                    y3s.append(sum_pred_2/count)
+                if target_size == 3: y3s.append(sum_pred_2/count)
 
                 sum_pred_0, sum_pred_1, sum_pred_2 = 0, 0, 0
                 count = 0
@@ -203,8 +215,7 @@ def inference(total_test_dataset, temps, prediction_test, target_size):
                 if target_size == 3:
                     y3s.append(sum_pred_2/count)
             else:
-                sum_pred_0, sum_pred_1, sum_pred_2 = __pred_count(
-                    sum_pred_0, sum_pred_1, sum_pred_2, prediction_test[i])
+                sum_pred_0, sum_pred_1, sum_pred_2 = __pred_count(sum_pred_0, sum_pred_1, sum_pred_2, prediction_test[i])
                 count += 1
     if target_size == 2:
         return np.array(xs), np.array(y1s), np.array(y2s)
@@ -220,3 +231,38 @@ def __pred_count(sum_pred_0, sum_pred_1, sum_pred_2, prediction_test):
     else:
         sum_pred_2 += 1
     return sum_pred_0, sum_pred_1, sum_pred_2
+
+
+def kronecker_delta(i, j):
+    if i == j:
+        return 1
+    else:
+        return 0
+
+
+def calc_correlation_configuration(array_list, L, model_name, Q=None):
+    """
+    行列からcorrelation configurationを構成するためのメソッド
+    """
+    def calc_gr(array_list, L, lx, ly, Q=None):
+        nlx = int(lx + int(L/2)) % L
+        nly = int(ly + int(L/2)) % L
+        if model_name == "2d_Ising":
+            gr = array_list[lx][ly]*array_list[nlx][ly] + \
+                 array_list[lx][ly]*array_list[lx][nly]
+        elif model_name == "2d_Potts":
+            gr = Q*(kronecker_delta(array_list[lx][ly], array_list[nlx][ly]) +
+                    kronecker_delta(array_list[lx][ly], array_list[lx][nly])) - 2
+            gr /= Q - 1
+        elif model_name == "2d_Clock":
+            gr = np.cos(2*np.pi*array_list[lx][ly]/Q - 2*np.pi*array_list[nlx][ly]/Q) + \
+                 np.cos(2*np.pi*array_list[lx][ly]/Q - 2*np.pi*array_list[lx][nly]/Q)
+        return gr
+
+    correlation_configuration = np.empty([L, L])
+    for lx in range(L):
+        for ly in range(L):
+            correlation_configuration[lx][ly] = calc_gr(
+                array_list, L, lx, ly, Q)
+            correlation_configuration[lx][ly] /= 2
+    return correlation_configuration
